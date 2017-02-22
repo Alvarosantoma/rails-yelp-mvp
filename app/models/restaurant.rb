c023ecd9b75e04bceb21e5536c96a74e7da4a549
alvaro <alvarosantoma@gmail.com>
2017-02-22 17:01:57 +0100

    green rake

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,5 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
   validates :category, inclusion: { in: ['chinese','belgian','italian','japanese','french','belgian','spanish'], allow_nil: false }
   validates :name, uniqueness: true, presence: true
   validates :address, presence: true
